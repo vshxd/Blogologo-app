48c950fd29bfd4837f3a8971df91279c5932a233
Vlad Shults <panda4keee@gmail.com>
2022-12-14 18:38:44 +0300

    fix(assets): fix icons/imgs names, add index.ts in assets folder

---
+++ src/assets/index.ts
@@ -0,0 +1,6 @@
+import { ReactComponent as LogoIcon } from "./img/logo.svg";
+import { ReactComponent as SearchIcon } from "./icons/search.svg";
+import UserIcon from "./icons/user-avatar.png";
+import UserLogoutIcon from "./icons/user-logout.png";
+
+export { LogoIcon, SearchIcon, UserIcon, UserLogoutIcon };
