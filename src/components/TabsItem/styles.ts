5cd968513371f6055c6760f689ce347ecf67b372
Vlad Shults <panda4keee@gmail.com>
2022-12-26 17:08:15 +0300

    feat(components): add transition for tabs

--- src/components/TabsItem/styles.ts
+++ src/components/TabsItem/styles.ts
@@ -7,6 +7,7 @@ export const Tab = styled.li<{ isActive: boolean }>`
   padding: 40px 40px 25px 40px;
   border-bottom: ${({ isActive }) =>
     isActive ? `2px solid ${Color.Primary}` : `2px solid ${Color.Border}`};
+  transition: all 0.1s linear;
 
   &:hover {
     color: ${({ isActive }) => (isActive ? `${Color.Primary}` : `${Color.Secondary_Light}`)};
